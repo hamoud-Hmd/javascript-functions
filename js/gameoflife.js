1a464ab561639ba1244115c8c6bc844377462214
HaMouD HMD <40148046+hamoud-Hmd@users.noreply.github.com>
2021-10-16 11:23:19 +0000

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,3 +1,88 @@
+function seed() {
+	return Array.prototype.slice.call(arguments);
+}
+
+function same([x, y], [j, k]) {
+	return x === j && y === k;
+}
+
+// The game state to search for `cell` is passed as the `this` value of the function.
+function contains(cell) {
+	return this.some(c => same(c, cell));
+}
+
+const printCell = (cell, state) => {
+	return contains.call(state, cell) ? '\u25A3' : '\u25A2';
+};
+
+const corners = (state = []) => {
+	if (state.length === 0) {
+		return {
+			topRight: [0, 0],
+			bottomLeft: [0, 0],
+		};
+	}
+
+	const xs = state.map(([x, _]) => x);
+	const ys = state.map(([_, y]) => y);
+
+	return {
+		topRight: [Math.max(...xs), Math.max(...ys)],
+		bottomLeft: [Math.min(...xs), Math.min(...ys)],
+	};
+};
+
+const printCells = state => {};
+
+const getNeighborsOf = ([x, y]) => {};
+
+const getLivingNeighbors = (cell, state) => {};
+
+const willBeAlive = (cell, state) => {};
+
+const calculateNext = state => {};
+
+const iterate = (state, iterations) => {};
+
+const main = (pattern, iterations) => {};
+
+const startPatterns = {
+	rpentomino: [
+		[3, 2],
+		[2, 3],
+		[3, 3],
+		[3, 4],
+		[4, 4],
+	],
+	glider: [
+		[-2, -2],
+		[-1, -2],
+		[-2, -1],
+		[-1, -1],
+		[1, 1],
+		[2, 1],
+		[3, 1],
+		[3, 2],
+		[2, 3],
+	],
+	square: [
+		[1, 1],
+		[2, 1],
+		[1, 2],
+		[2, 2],
+	],
+};
+
+const [pattern, iterations] = process.argv.slice(2);
+const runAsScript = require.main === module;
+
+if (runAsScript) {
+	if (startPatterns[pattern] && !isNaN(parseInt(iterations))) {
+		main(pattern, parseInt(iterations));
+	} else {
+		console.log('Usage: node js/gameoflife.js rpentomino 50');
+	}
+}
 exports.seed = seed;
 exports.same = same;
 exports.contains = contains;
